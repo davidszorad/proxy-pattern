e23504e3cb0c3aa49a88b5ff12ed5abeccff90f0
Szorad <david.szorad@accenture.com>
2017-09-21 15:01:57 +0200

    Initial commit

---
+++ Structural/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyPattern.Structural
+{
+    /*
+    Provide a surrogate or placeholder for another object to control access to it.
+        
+    This structural code demonstrates the Proxy pattern which provides 
+    a representative object (proxy) that controls access to another similar object.
+        
+    OUTPUT:
+    Called RealSubject.Request()
+    */
+
+    /// <summary>
+    /// MainApp startup class for Structural
+    /// Proxy Design Pattern.
+    /// </summary>
+    class Main
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void MainApp()
+        {
+            // Create proxy and request a service
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
